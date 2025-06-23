ad6bdfeffeea6327758faefc5fe4ca0612dd2b3b
Deepak <coding.deepak12@outlook.com>
2025-06-23 12:45:05 +0530

    Modify structure and add basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const weatherForm = document.querySelector('.weather-form');
+const cityInput = document.querySelector('.city-input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const city = cityInput.value.trim();
+  if (city) {
+    console.log('City entered: ', city);
+  } else {
+    console.log('Please enter a city');
+  }
+});
